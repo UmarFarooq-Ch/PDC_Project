ef3f2901abfb2f2283deb4099499fdc57dd1823a
Umar Farooq <umarch@live.com>
2019-05-08 22:58:11 +0500

    beta v4 -- Load balancer added

--- Server/server.go
+++ Server/server.go
@@ -65,6 +65,32 @@ func resetResponses(slaveSlice *[]Slave) {
 
 }
 
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func unique(s []string) []string {
+	var m = map[string]int{}
+	for _, value := range s {
+		m[value]++
+	}
+	for key := range m {
+		if m[key] != 1 {
+			delete(m, key)
+		}
+	}
+	var uniqueSlice []string
+	for key := range m {
+		uniqueSlice = append(uniqueSlice, key)
+	}
+	return uniqueSlice
+}
+
 //-------End of Util func--------
 
 //Slave is something
@@ -234,27 +260,138 @@ func handleSlaves(msgchan chan Msg, addchan chan Slave, reqchan, rreqChan chan s
 				if len(fNameMap) != 0 && workingOn != "" {
 					findingInReplica = true
 					var ind []int
+					var files = map[int]map[string]int{}
 					for i, slave := range slaveSlice {
 						itContains := false
-						command := "rep:" + splitworkingon[0] + ":"
+						//command := "rep:" + splitworkingon[0] + ":"
 						for _, filename := range slave.rchunks {
 							if _, ok := fNameMap[filename]; ok {
-								command = command + "," + filename
+								if !itContains {
+									files[i] = map[string]int{}
+								}
+								files[i][filename] = 0 //command = command + "," + filename
 								itContains = true
 							}
 						}
 						if itContains {
-							//send this command and reset response
-							slave.response = "nr"
+							// send this command and reset response
+							// slave.response = "nr"
 							ind = append(ind, i)
 							findingInReplica = true
-							// fmt.Println("slave: ", slaveSlice)
-							slave.channel <- command
 						}
 					}
-					for _, i := range ind {
-						slaveSlice[i].response = "nr"
+					//load balancer
+					//first schedule slaves having 1 chunk only
+					//if 2 slaves have same 1 chunk, then it will schedule on 1st slave
+					var ready_command = map[int]map[string]int{}
+					var scheduled []string //all chunks which are scheduled
+					again := true
+					for again {
+						again = false
+						for i, m := range files {
+							if len(m) == 1 {
+								for key := range m {
+									if !contains(scheduled, key) {
+										if ready_command[i] == nil {
+											ready_command[i] = map[string]int{}
+										}
+										ready_command[i][key] = 0
+										scheduled = append(scheduled, key)
+										again = true
+										delete(files[i], key)
+									}
+								}
+							}
+						}
+						// now find unique chunks
+						//get all chunks in array (except already scheduled) and find unique
+						var allchunks []string
+						for _, m := range files {
+							for key := range m {
+								if !contains(scheduled, key) {
+									allchunks = append(allchunks, key)
+								}
+							}
+						}
+						uniqueChunks := unique(allchunks)
+						//now add these chunks into map ready_command
+						for _, uniqueChunk := range uniqueChunks {
+							//find index of slave which contain this chunk
+							for i, m := range files {
+								for key := range m {
+									if key == uniqueChunk {
+										//initialize inner map if not initialized
+										if ready_command[i] == nil {
+											ready_command[i] = map[string]int{}
+										}
+										ready_command[i][key] = 0
+										scheduled = append(scheduled, key)
+										again = true
+										delete(files[i], key)
+									}
+								}
+							}
+						}
 					}
+					//now add remaining schedules
+					//first make following map
+					//{ chunk_0 : [1,2,3]} chunk0 is present in 1, 2, 3 slave
+					chunkTracker := make(map[string][]int)
+					for i, m := range files {
+						for key := range m {
+							if !contains(scheduled, key) {
+								chunkTracker[key] = append(chunkTracker[key], i)
+							}
+						}
+					}
+					//now select each chuck and
+					//check which slave number has less schedules in map ready_command
+					for chunkName, lst := range chunkTracker {
+						firstTime := true
+						var SNWHLS int //SNWHLS == Slave number which has less schedules
+						var NOS int    //Number of schedules
+						//SNFL == slave number from list
+						for _, SNFL := range lst {
+							if firstTime {
+								if ready_command[SNFL] == nil {
+									SNWHLS = SNFL
+									break
+								}
+								SNWHLS = SNFL
+								NOS = len(ready_command[SNFL])
+								firstTime = false
+							} else {
+								if NOS > len(ready_command[SNFL]) {
+									NOS = len(ready_command[SNFL])
+									SNWHLS = SNFL
+								}
+							}
+						}
+						//Now give search command to slave number == SNWHLS
+						if ready_command[SNWHLS] == nil {
+							ready_command[SNWHLS] = map[string]int{}
+						}
+						ready_command[SNWHLS][chunkName] = 0
+						scheduled = append(scheduled, chunkName)
+					}
+
+					//now send commands
+					for slavenumber, m := range ready_command {
+						command := "rep:" + splitworkingon[0] + ":"
+						if len(m) > 0 {
+							slaveSlice[slavenumber].response = "nr"
+
+							for key := range m {
+								command = command + "," + key
+							}
+							slaveSlice[slavenumber].channel <- command
+						}
+					}
+					// for _, i := range ind {
+					// 	slaveSlice[i].response = "nr"
+					// 	//todo send command
+					// 	// slave.channel <- command
+					// }
 				} else if workingOn != "" {
 					resetResponses(&slaveSlice)
 					findingInReplica = false
@@ -300,6 +437,7 @@ func handleNewSlaves(port string, reqChan, rreqChan chan string) {
 }
 
 func main() {
+
 	log.Print("To run client server on 3000 use cport=3000")
 	log.Print("To run slave  server on 6000 use sport=9000")
 	args := os.Args
@@ -315,9 +453,38 @@ func main() {
 		}
 	}
 
+	//initialize maps array
+
 	reqChan := make(chan string, 5)
 	rreqChan := make(chan string, 5) //response of reqChan
 
 	go handleNewSlaves(sport, reqChan, rreqChan)
 	client.ClientServer(cport, reqChan, rreqChan)
 }
+
+// var m = map[int]map[string]int{}
+// m[1] = map[string]int{}
+// // m[0] = map[string]int{}
+// m[1]["chunk_2"] = 0
+// m[1]["chunk_3"] = 0
+// m[2] = map[string]int{}
+// m[2]["chunk_2"] = 0
+// m[2]["chunk_3"] = 0
+// m[2]["chunk_4"] = 0
+
+// for i, m := range m {
+// 	fmt.Println(i, m)
+// }
+// fmt.Println(len(m[2]))
+// fmt.Println(m[0] == nil)
+// var ss []string //{"1", "2","3","4","3","4" }
+// ss = append(ss, "1")
+// ss = append(ss, "2")
+// ss = append(ss, "3")
+// ss = append(ss, "4")
+// ss = append(ss, "1")
+// ss = append(ss, "2")
+// ss = append(ss, "3")
+// ss = append(ss, "7")
+
+// fmt.Println(unique(ss))
